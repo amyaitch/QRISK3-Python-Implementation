b3f283d9429f93b4f3375684700ac2373dbfa6cf
amyaitch <amyaitchison@outlook.com>
2025-07-17 19:07:24 +0100

    Update identify_SGLT2i.py

--- QRISK3/Identify SGLT2i/identify_SGLT2i.py
+++ QRISK3/Identify SGLT2i/identify_SGLT2i.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import os
 
 def identify_sglt2i_prescriptions(chunk):
     """
@@ -12,25 +13,21 @@ def identify_sglt2i_prescriptions(chunk):
         pd.DataFrame: A dataframe containing only the SGLT2i prescriptions.
     """
     sglt2i_drugs = ["Canagliflozin", "Dapagliflozin", "Empagliflozin", "Ertugliflozin"]
-    # The drug name is in the 'drug' column. 
-    # The regular expression below will look for any of the drugs in the sglt2i_drugs list, case-insensitive.
     return chunk[chunk['drug'].str.contains('|'.join(sglt2i_drugs), case=False, na=False)]
 
-# Create a new csv file to store the results
-output_filename = '/Users/amyaitchison/Desktop/MSc/Project/Practice/sglt2i_prescriptions.csv'
+if __name__ == "__main__":
+    # Define input and output paths using relative directories
+    data_dir = os.path.join(os.path.dirname(__file__), 'data')
+    input_filename = os.path.join(data_dir, 'full_prescriptions.csv')
+    output_filename = os.path.join(data_dir, 'sglt2i_prescriptions.csv')
 
-# Process the large CSV file in chunks
-chunksize = 10 ** 6  # 1 million rows at a time
-first_chunk = True
-for chunk in pd.read_csv('/Users/amyaitchison/Desktop/MSc/Project/Practice/full_prescriptions.csv', chunksize=chunksize):
-    sglt2i_chunk = identify_sglt2i_prescriptions(chunk)
-    if not sglt2i_chunk.empty:
-        if first_chunk:
-            # For the first chunk, write with header
-            sglt2i_chunk.to_csv(output_filename, index=False, mode='w', header=True)
+    # Process the large CSV file in chunks
+    chunksize = 10 ** 6  # 1 million rows at a time
+    first_chunk = True
+    for chunk in pd.read_csv(input_filename, chunksize=chunksize):
+        sglt2i_chunk = identify_sglt2i_prescriptions(chunk)
+        if not sglt2i_chunk.empty:
+            sglt2i_chunk.to_csv(output_filename, index=False, mode='w' if first_chunk else 'a', header=first_chunk)
             first_chunk = False
-        else:
-            # For subsequent chunks, append without header
-            sglt2i_chunk.to_csv(output_filename, index=False, mode='a', header=False)
 
-print(f"SGLT2i prescriptions saved to {output_filename}")
+    print(f"SGLT2i prescriptions saved to {output_filename}")
